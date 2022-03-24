ad22708a6d5056191f813ae7e5dc04c60d1cedf5
Darrell Tisdale <Darrell.Tisdale@outlook.com>
2022-03-24 12:27:09 -0400

    fix module errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Fragment, useEffect } from "react";
-import { Switch, Route, Redirect, useLocation } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { getUser } from "./redux/userSlice/apiCalls";
 import { getPlayLists } from "./redux/playListSlice/apiCalls";
@@ -19,72 +19,72 @@ import LikedSongs from "./pages/LikedSongs";
 import Profile from "./pages/Profile";
 
 const App = () => {
-	const dispatch = useDispatch();
-	const location = useLocation();
-	const { user } = useSelector((state) => state.auth);
-	const { currentSong } = useSelector((state) => state.audioPlayer);
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const { user } = useSelector((state) => state.auth);
+  const { currentSong } = useSelector((state) => state.audioPlayer);
 
-	useEffect(() => {
-		let token = null;
-		const root = JSON.parse(window.localStorage.getItem("persist:root"));
+  useEffect(() => {
+    let token = null;
+    const root = JSON.parse(window.localStorage.getItem("persist:root"));
 
-		if (root) {
-			const { auth } = root;
-			const { user } = JSON.parse(auth);
-			if (user) token = user.token;
-		}
+    if (root) {
+      const { auth } = root;
+      const { user } = JSON.parse(auth);
+      if (user) token = user.token;
+    }
 
-		if (user && token) {
-			getUser(user._id, dispatch);
-			getPlayLists(dispatch);
-		}
-	}, [dispatch, user]);
+    if (user && token) {
+      getUser(user._id, dispatch);
+      getPlayLists(dispatch);
+    }
+  }, [dispatch, user]);
 
-	return (
-		<Fragment>
-			{user &&
-				location.pathname !== "/login" &&
-				location.pathname !== "/" &&
-				location.pathname !== "/signup" &&
-				location.pathname !== "/not-found" && (
-					<Fragment>
-						<Navbar />
-						<Sidebar />
-						{currentSong && <AudioPlayer />}
-					</Fragment>
-				)}
-			<Switch>
-				<Route exact path="/" component={Main} />
-				<PrivateRoute exact user={user} path="/home" component={Home} />
-				<PrivateRoute
-					exact
-					user={user}
-					path="/collection/tracks"
-					component={LikedSongs}
-				/>
-				<PrivateRoute
-					exact
-					user={user}
-					path="/collection/playlists"
-					component={Library}
-				/>
-				<PrivateRoute exact user={user} path="/search" component={Search} />
-				<PrivateRoute
-					exact
-					user={user}
-					path="/playlist/:id"
-					component={Playlist}
-				/>
-				<PrivateRoute exact user={user} path="/me" component={Profile} />
-				{user && <Redirect from="/signup" to="/home" />}
-				{user && <Redirect from="/login" to="/home" />}
-				<Route path="/signup" component={SignUp} />
-				<Route path="/login" component={Login} />
-				<Route path="/not-found" component={NotFound} />
-				<Redirect to="/not-found" />
-			</Switch>
-		</Fragment>
-	);
+  return (
+    <Fragment>
+      {user &&
+        location.pathname !== "/login" &&
+        location.pathname !== "/" &&
+        location.pathname !== "/signup" &&
+        location.pathname !== "/not-found" && (
+          <Fragment>
+            <Navbar />
+            <Sidebar />
+            {currentSong && <AudioPlayer />}
+          </Fragment>
+        )}
+      <Routes>
+        <Route exact path="/" component={Main} />
+        <PrivateRoute exact user={user} path="/home" component={Home} />
+        <PrivateRoute
+          exact
+          user={user}
+          path="/collection/tracks"
+          component={LikedSongs}
+        />
+        <PrivateRoute
+          exact
+          user={user}
+          path="/collection/playlists"
+          component={Library}
+        />
+        <PrivateRoute exact user={user} path="/search" component={Search} />
+        <PrivateRoute
+          exact
+          user={user}
+          path="/playlist/:id"
+          component={Playlist}
+        />
+        <PrivateRoute exact user={user} path="/me" component={Profile} />
+        {user && <Navigate from="/signup" to="/home" />}
+        {user && <Navigate from="/login" to="/home" />}
+        <Route path="/signup" component={SignUp} />
+        <Route path="/login" component={Login} />
+        <Route path="/not-found" component={NotFound} />
+        <Navigate to="/not-found" />
+      </Routes>
+    </Fragment>
+  );
 };
 
 export default App;
